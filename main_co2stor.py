bed4ea5230025f29f68db21b23648b29929b6885
Luca <l.bertoni@uu.nl>
2024-07-08 11:13:43 +0200

    Trying to fix TPWL calculations

--- main_co2stor.py
+++ main_co2stor.py
@@ -58,7 +58,7 @@ adopt.fill_carrier_data(path, value_or_data=20000, columns=['Import limit'], car
 
 # Construct and solve the model
 m = adopt.ModelHub()
-m.read_data(path, start_period=0, end_period=4)
+m.read_data(path, start_period=0, end_period=6)
 m.quick_solve()
 m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
     "PermanentStorage_CO2_detailed"].pprint()
