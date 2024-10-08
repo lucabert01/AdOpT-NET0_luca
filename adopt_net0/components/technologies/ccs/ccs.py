3e18845553aab2ec79cd6fc34187b01575ea6aa9
lucabert01 <l.bertoni@uu.nl>
2024-10-08 13:44:15 +0200

    Added plot_results.py

--- adopt_net0/components/technologies/ccs/ccs.py
+++ adopt_net0/components/technologies/ccs/ccs.py
@@ -39,13 +39,15 @@ def fit_ccs_coeff(co2_concentration: float, ccs_data: dict, climate_data: pd.Dat
     ccs_data = ModelComponent(ccs_data)
 
     molar_mass_CO2 = 44.01
+    # TODO: retrieve timestep_length from Topology instead of wrtining it here
+    timestep_length = 24
 
     # Recalculate min/max size to have it in t/hCO2_in
     ccs_data.input_parameters.size_min = (
-        ccs_data.input_parameters.size_min * co2_concentration
+        ccs_data.input_parameters.size_min * co2_concentration * timestep_length
     )
     ccs_data.input_parameters.size_max = (
-        ccs_data.input_parameters.size_max * co2_concentration
+        ccs_data.input_parameters.size_max * co2_concentration*timestep_length
     )
 
     # Calculate input ratios
