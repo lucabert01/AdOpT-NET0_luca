6d166161ef492a52e650dbdd9ddff22e7cdf89c3
Luca <l.bertoni@uu.nl>
2024-10-17 16:43:46 +0200

    Commit before update

--- data_simulations_co2stor_linear/plot_results.py
+++ data_simulations_co2stor_linear/plot_results.py
@@ -2,30 +2,28 @@ import h5py
 from pathlib import Path
 from adopt_net0.result_management.read_results import (
     print_h5_tree,
-    extract_dataset_from_h5,
     extract_datasets_from_h5group,
 )
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
 import numpy as np
-import matplotlib.cm as cm
 
 
 
-file_path = Path(__file__).parent.parent/"userData/20241016232530-1/optimization_results.h5"
+file_path = Path(__file__).parent.parent/"userData/20241017162052-1/optimization_results.h5"
 
 
 print_h5_tree(file_path)
 
 with h5py.File(file_path, 'r') as hdf_file:
     df_operation = extract_datasets_from_h5group(hdf_file["operation"])
-    df_design = extract_datasets_from_h5group(hdf_file["design/nodes/period1/storage"])
+    df_design = extract_datasets_from_h5group(hdf_file["design/nodes/period1"])
 
 print(df_operation)
 
-cement_output_df = df_operation[('technology_operation', 'period1', 'storage', 'CementEmitter')]
-w2e_output_df = df_operation[('technology_operation', 'period1', 'storage', 'WasteToEnergyEmitter')]
+cement_output_df = df_operation[('technology_operation', 'period1', 'industrial_cluster', 'CementEmitter')]
+w2e_output_df = df_operation[('technology_operation', 'period1', 'industrial_cluster', 'WasteToEnergyEmitter')]
 co2stor_results_df = df_operation[("technology_operation", "period1","storage", "PermanentStorage_CO2_detailed")]
 
 emission_cement = cement_output_df['cement_output']
@@ -37,9 +35,9 @@ whp = co2stor_results_df['whp']
 average_inj_rate = co2stor_results_df['average_inj_rate']
 storage_level = co2stor_results_df['storage_level']
 power_pump = co2stor_results_df['electricity_input']
-size_pump = df_design[('PermanentStorage_CO2_detailed','size_pump')]
-size_ccs_cement = df_design[('CementEmitter','size_ccs')]
-size_ccs_w2e = df_design[('WasteToEnergyEmitter','size_ccs')]
+size_pump = df_design[('storage','PermanentStorage_CO2_detailed','size_pump')]
+size_ccs_cement = df_design[('industrial_cluster','CementEmitter','size_ccs')]
+size_ccs_w2e = df_design[('industrial_cluster','WasteToEnergyEmitter','size_ccs')]
 
 emission_tot = emission_w2e + emission_cement
 tot_co2_captured = co2_captured_cement +co2_captured_w2e
@@ -112,29 +110,29 @@ ratio_fit_pump = power_pump/pump_unfitted_power
 fixed_pump_power = pump_unfitted_power[0]
 
 
-plt.figure(figsize=(10, 6))
-plt.plot(days/365, ratio_fit_pump, color='#294B6C', linewidth=2, label='Fitted pump power over real one')
-plt.axhline(y=1.0, color="#DCE391", linestyle='--', linewidth=1, label='Perfect fit')
-plt.xlabel('Time [year]')
-plt.ylabel('Ratio')
-plt.ylim(0.6, max(ratio_fit_pump) * 1.1)
-plt.title('Quality of the pump fit')
-plt.legend()
-plt.tight_layout()
-plt.show(block=False)
-plt.savefig(path_plot/"fit_pump.jpg", format='jpeg', dpi=500)
-
-
-plt.figure(figsize=(10, 6))
-plt.plot(days/365, power_pump/fixed_pump_power, color='#66B2A5', linewidth=2, label='With model')
-plt.axhline(y=1.0, color="#082D48", linestyle='--', linewidth=1, label='Without model')
-plt.xlabel('Time [year]')
-plt.ylabel('Normalized specific pump consumption')
-plt.ylim(0, max(power_pump/fixed_pump_power)*1.1)
-plt.title('Impact of bhp variations on the pump power consumption')
-plt.legend()
-plt.tight_layout()
-plt.show(block=False)
-plt.savefig(path_plot/"power_pump.jpg", format='jpeg', dpi=500)
+# plt.figure(figsize=(10, 6))
+# plt.plot(days/365, ratio_fit_pump, color='#294B6C', linewidth=2, label='Fitted pump power over real one')
+# plt.axhline(y=1.0, color="#DCE391", linestyle='--', linewidth=1, label='Perfect fit')
+# plt.xlabel('Time [year]')
+# plt.ylabel('Ratio')
+# plt.ylim(0.6, max(ratio_fit_pump) * 1.1)
+# plt.title('Quality of the pump fit')
+# plt.legend()
+# plt.tight_layout()
+# plt.show(block=False)
+# plt.savefig(path_plot/"fit_pump.jpg", format='jpeg', dpi=500)
+#
+#
+# plt.figure(figsize=(10, 6))
+# plt.plot(days/365, power_pump/fixed_pump_power, color='#66B2A5', linewidth=2, label='With model')
+# plt.axhline(y=1.0, color="#082D48", linestyle='--', linewidth=1, label='Without model')
+# plt.xlabel('Time [year]')
+# plt.ylabel('Normalized specific pump consumption')
+# plt.ylim(0, max(power_pump/fixed_pump_power)*1.1)
+# plt.title('Impact of bhp variations on the pump power consumption')
+# plt.legend()
+# plt.tight_layout()
+# plt.show(block=False)
+# plt.savefig(path_plot/"power_pump.jpg", format='jpeg', dpi=500)
 
 plt.show()
--- main_co2stor.py
+++ main_co2stor.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 import pyomo.environ as pyo
 import scipy.io as sci
+import os
 
 # Function for later
 def prepare_data_series(array, type, end_period):
@@ -27,7 +28,7 @@ adopt.create_optimization_templates(path)
 with open(path / "Topology.json", "r") as json_file:
     topology = json.load(json_file)
 # Nodes
-topology["nodes"] = ["storage"]
+topology["nodes"] = ["storage", "industrial_cluster"]
 # Carriers:
 topology["carriers"] = ["electricity", "CO2captured", "cement", "heat", "waste"]
 # Investment periods:
@@ -36,7 +37,7 @@ topology["investment_periods"] = ["period1"]
 with open(path / "Topology.json", "w") as json_file:
     json.dump(topology, json_file, indent=4)
 
-end_period = 10*180
+end_period = 3*180
 
 
 # Load json template
@@ -55,29 +56,78 @@ adopt.create_input_data_folder_template(path)
 # Add technologies
 with open(path / "period1" / "node_data" / "storage" / "Technologies.json", "r") as json_file:
     technologies = json.load(json_file)
-technologies["new"] = ["PermanentStorage_CO2_detailed", "CementEmitter", "WasteToEnergyEmitter"]
+technologies["new"] = ["PermanentStorage_CO2_detailed"]
 
 with open(path / "period1" / "node_data" / "storage" / "Technologies.json", "w") as json_file:
+    json.dump(technologies, json_file, indent=4)
+
+with open(path / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "r") as json_file:
+    technologies = json.load(json_file)
+technologies["new"] = ["CementEmitter", "WasteToEnergyEmitter"]
+
+with open(path / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "w") as json_file:
     json.dump(technologies, json_file, indent=4)
 
 # Copy over technology files
 adopt.copy_technology_data(path)
 
+
+# Add networks
+with open(path / "period1" / "Networks.json", "r") as json_file:
+    networks = json.load(json_file)
+networks["new"] = ["CO2PipelineOnshore"]
+
+with open(path / "period1" / "Networks.json", "w") as json_file:
+    json.dump(networks, json_file, indent=4)
+
+# Make a new folder for the new network
+os.makedirs(path / "period1" / "network_topology" / "new" / "CO2PipelineOnshore", exist_ok=True)
+# max size arc
+arc_size = pd.read_csv(path / "period1" / "network_topology" / "new" / "size_max_arcs.csv", sep=";", index_col=0)
+arc_size.loc["industrial_cluster", "storage"] = 10000
+arc_size.to_csv(path / "period1" / "network_topology" / "new" / "CO2PipelineOnshore" / "size_max_arcs.csv", sep=";")
+print("Max size per arc:", arc_size)
+
+# Use the templates, fill and save them to the respective directory
+# Connection
+connection = pd.read_csv(path / "period1" / "network_topology" / "new" / "connection.csv", sep=";", index_col=0)
+connection.loc["industrial_cluster", "storage"] = 1
+connection.to_csv(path / "period1" / "network_topology" / "new" / "CO2PipelineOnshore" / "connection.csv", sep=";")
+print("Connection:", connection)
+
+# Delete the template
+os.remove(path / "period1" / "network_topology" / "new" / "connection.csv")
+
+# Distance
+distance = pd.read_csv(path / "period1" / "network_topology" / "new" / "distance.csv", sep=";", index_col=0)
+distance.loc["industrial_cluster", "storage"] = 100
+distance.to_csv(path / "period1" / "network_topology" / "new" / "CO2PipelineOnshore" / "distance.csv", sep=";")
+print("Distance:", distance)
+
+# Delete the template
+os.remove(path / "period1" / "network_topology" / "new" / "distance.csv")
+
+# Delete the max_size_arc template
+os.remove(path / "period1" / "network_topology" / "new" / "size_max_arcs.csv")
+
+
+
+
+
+
 # Load the .mat file for ROM model of saline aquifer
-general_performance_data_path = Path(__file__).parent
-aquifer_performance_data_path = (
-        general_performance_data_path
-        / "adopt_net0/data/technology_data/Sink/SalineAquifer_data/matrices_for_ROM.mat"
-)
-
-
-co2stor_data = sci.loadmat(aquifer_performance_data_path)
-inj_rate = co2stor_data["uNew"]
-inj_rate = inj_rate[:end_period, 0]
-conversion_factor = 550*3.6
-tot_inj_rate = np.zeros(8760)
-tot_inj_rate[:len(inj_rate)] = inj_rate*conversion_factor
-tot_inj_rate = pd.DataFrame(tot_inj_rate, columns=['Import limit'])
+# general_performance_data_path = Path(__file__).parent
+# aquifer_performance_data_path = (
+#         general_performance_data_path
+#         / "adopt_net0/data/technology_data/Sink/SalineAquifer_data/matrices_for_ROM.mat"
+# )
+# co2stor_data = sci.loadmat(aquifer_performance_data_path)
+# inj_rate = co2stor_data["uNew"]
+# inj_rate = inj_rate[:end_period, 0]
+# conversion_factor = 550*3.6
+# tot_inj_rate = np.zeros(8760)
+# tot_inj_rate[:len(inj_rate)] = inj_rate*conversion_factor
+# tot_inj_rate = pd.DataFrame(tot_inj_rate, columns=['Import limit'])
 
 # Load and prepare data series
 data_path = Path(__file__).parent/"data_simulations_co2stor_linear"
@@ -89,18 +139,16 @@ carbon_price_timeseries = np.load(data_path/'fullprofile_co2_price.npy', allow_p
 carbon_price_timeseries = prepare_data_series(carbon_price_timeseries, "price", end_period)
 
 # Set import limits/cost
-adopt.fill_carrier_data(path, value_or_data=0, columns=['Import limit'], carriers=['CO2captured'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=-1500, columns=['Import price'], carriers=['CO2captured'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=5000, columns=['Import limit'], carriers=['electricity'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=20000, columns=['Import limit'], carriers=['heat'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=100, columns=['Import price'], carriers=['electricity'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=40, columns=['Import price'], carriers=['heat'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=cement_emissions/3, columns=['Demand'], carriers=['cement'], nodes=['storage'])
-adopt.fill_carrier_data(path, value_or_data=waste_emissions/2, columns=['Demand'], carriers=['waste'], nodes=['storage'])
+adopt.fill_carrier_data(path, value_or_data=5000, columns=['Import limit'], carriers=['electricity'], nodes=['industrial_cluster','storage'])
+adopt.fill_carrier_data(path, value_or_data=20000, columns=['Import limit'], carriers=['heat'], nodes=['industrial_cluster','storage'])
+adopt.fill_carrier_data(path, value_or_data=100, columns=['Import price'], carriers=['electricity'], nodes=['industrial_cluster','storage'])
+adopt.fill_carrier_data(path, value_or_data=40, columns=['Import price'], carriers=['heat'], nodes=['industrial_cluster','storage'])
+adopt.fill_carrier_data(path, value_or_data=cement_emissions/3, columns=['Demand'], carriers=['cement'], nodes=['industrial_cluster'])
+adopt.fill_carrier_data(path, value_or_data=waste_emissions/2, columns=['Demand'], carriers=['waste'], nodes=['industrial_cluster'])
 
 carbon_price = carbon_price_timeseries['price'].values
-carbon_price = np.linspace(90, 300, 8760)
-carbon_cost_path = path / "period1" / "node_data" / "storage" /"CarbonCost.csv"
+carbon_price = np.linspace(110, 300, 8760)
+carbon_cost_path = path / "period1" / "node_data" / "industrial_cluster" /"CarbonCost.csv"
 carbon_cost_template = pd.read_csv(carbon_cost_path, sep=';', index_col=0, header=0)
 carbon_cost_template['price'] = carbon_price
 carbon_cost_template = carbon_cost_template.reset_index()
@@ -111,50 +159,29 @@ m = adopt.ModelHub()
 m.read_data(path, start_period=0, end_period=end_period)
 m.construct_model()
 m.construct_balances()
-
-# # Force output of CementEmitter
-# model = m.model[m.info_solving_algorithms["aggregation_model"]].periods["period1"]
-# set_t_full = model.set_t_full
-# emission_profile_cement = np.ones((8760,1))*50
-# b_tec = m.model[m.info_solving_algorithms[
-#     "aggregation_model"]].periods["period1"].node_blocks["storage"].tech_blocks_active[
-#     "CementEmitter"]
-#
-# def init_cement_emissions(const,t):
-#     return b_tec.var_output[t, "cement"] == emission_profile_cement[t]
-# b_tec.const_emission_cement = pyo.Constraint(set_t_full, rule=init_cement_emissions)
-
 m.solve()
-# m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
-#     "PermanentStorage_CO2_detailed"].var_distance.pprint()
-# m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
-#     "PermanentStorage_CO2_detailed"].var_bhp.pprint()
-# m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
-#     "PermanentStorage_CO2_detailed"].var_d_min.pprint()
 
 
-compare_co2_stored = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
+
+co2_stored = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
     "PermanentStorage_CO2_detailed"].var_input[t, "CO2captured"].value for t in range(1,end_period))
-compare_el_storage = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
+el_storage = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
     "PermanentStorage_CO2_detailed"].var_input[t, "electricity"].value for t in range(1,end_period))
-compare_heat_ccs = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
-    "CementEmitter"].var_input_ccs[t, "heat"].value for t in range(1,end_period))
-compare_el_ccs = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
-    "CementEmitter"].var_input_ccs[t, "electricity"].value for t in range(1,end_period))
-compare_co2_ccs = sum(m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
+co2_ccs_cement = sum(m.model["full"].periods["period1"].node_blocks["industrial_cluster"].tech_blocks_active[
     "CementEmitter"].var_output_ccs[t, "CO2captured"].value for t in range(1,end_period))
-compare_size_ccs = m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
+size_ccs_cement = m.model["full"].periods["period1"].node_blocks["industrial_cluster"].tech_blocks_active[
     "CementEmitter"].var_size_ccs.value
-compare_capex_ccs = m.model["full"].periods["period1"].node_blocks["storage"].tech_blocks_active[
-    "CementEmitter"].var_capex_ccs.value
+co2_ccs_w2e = sum(m.model["full"].periods["period1"].node_blocks["industrial_cluster"].tech_blocks_active[
+    "WasteToEnergyEmitter"].var_output_ccs[t, "CO2captured"].value for t in range(1,end_period))
+size_ccs_w2e = m.model["full"].periods["period1"].node_blocks["industrial_cluster"].tech_blocks_active[
+    "WasteToEnergyEmitter"].var_size_ccs.value
 
 print("Some results:")
-print(f"CO2 Stored: {compare_co2_stored:.2f}")
-print(f"Electricity for Storage: {compare_el_storage:.2f}")
-print(f"Heat for CCS: {compare_heat_ccs:.2f}")
-print(f"Electricity for CCS: {compare_el_ccs:.2f}")
-print(f"CO2 captured CCS: {compare_co2_ccs:.2f}")
-print(f"CapEx for CCS: {compare_capex_ccs:.2f}")
-print(f"Size CCS: {compare_size_ccs:.2f}")
+print(f"CO2 Stored: {co2_stored:.2f}")
+print(f"Electricity for Storage: {el_storage:.2f}")
+print(f"CO2 captured cement: {co2_ccs_cement:.2f}")
+print(f"Size CCS cement: {size_ccs_cement:.2f}")
+print(f"CO2 captured W2E: {co2_ccs_w2e:.2f}")
+print(f"Size CCS W2E: {size_ccs_w2e:.2f}")
 
 
