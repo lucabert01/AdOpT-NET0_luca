2ced6ea5f7b326282831a976146f6db394b6a193
lucabert01 <l.bertoni@uu.nl>
2024-12-10 15:43:58 +0100

    results

--- data_simulations_co2stor_linear/plot_results.py
+++ data_simulations_co2stor_linear/plot_results.py
@@ -44,7 +44,7 @@ def save_figure_for_paper(fig, filename, file_path_results):
 
     print(f"Figure saved as {filename}.pdf and {filename}.jpg in {file_path_results}")
 
-file_path = Path(__file__).parent.parent/"userData/20241209154811-1/optimization_results.h5"
+file_path = Path(__file__).parent.parent/"userData/FullRun20241210/optimization_results.h5"
 
 
 print_h5_tree(file_path)
@@ -95,40 +95,40 @@ file_path_results = r"C:\Users\0954659\OneDrive - Universiteit Utrecht\Documents
 cement_ratio = co2_captured_cement / emission_cement
 w2e_ratio = co2_captured_w2e / emission_w2e
 
-# Plot the ratios
-plt.figure(figsize=(12, 6))
-plt.plot(cement_ratio, label='Cement: CO2 Captured/Emissions', marker='o', markersize=3, linewidth=0.7)
-plt.plot(w2e_ratio, label='W2E: CO2 Captured/Emissions', marker='x', linestyle='--', markersize=3, linewidth=0.7)
-plt.xlabel('Time')
-plt.ylabel('CO2 Captured/Emissions Ratio')
-plt.title('Comparison of CO2 Captured to Emissions Ratios')
-plt.xlim(0, 1800)
-plt.legend()
-
-plt.figure(figsize=(12, 6))
-fig, ax1 = plt.subplots(figsize=(12, 6))
-line1 = ax1.plot((co2_captured_cement + co2_captured_w2e) / (emission_cement + emission_w2e),
-                 label='CO2 Captured/Total Emissions', color='blue', marker='o', markersize=3, linewidth=0.7)
-ax1.set_xlabel('Time')
-ax1.set_ylabel('CO2 Captured/Total Emissions', color='blue')
-ax1.tick_params(axis='y', labelcolor='blue')
-ax2 = ax1.twinx()
-line2 = ax2.plot((co2_captured_cement + co2_captured_w2e),
-                 label='Total CO2 Captured', color='green', marker='x', markersize=3, linewidth=0.7, linestyle='--')
-ax2.set_ylabel('Total CO2 Captured', color='green')
-ax2.tick_params(axis='y', labelcolor='green')
-plt.title('Share of Total CO2 Captured')
-ax1.grid(True)
-lines = line1 + line2
-labels = [line.get_label() for line in lines]
-ax1.legend(lines, labels, loc='lower right')
-ax1.set_xlim(0, 1800)
-plt.show()
+# # Plot the ratios
+# plt.figure(figsize=(12, 6))
+# plt.plot(cement_ratio, label='Cement: CO2 Captured/Emissions', marker='o', markersize=3, linewidth=0.7)
+# plt.plot(w2e_ratio, label='W2E: CO2 Captured/Emissions', marker='x', linestyle='--', markersize=3, linewidth=0.7)
+# plt.xlabel('Time')
+# plt.ylabel('CO2 Captured/Emissions Ratio')
+# plt.title('Comparison of CO2 Captured to Emissions Ratios')
+# plt.xlim(0, 1800)
+# plt.legend()
+#
+# plt.figure(figsize=(12, 6))
+# fig, ax1 = plt.subplots(figsize=(12, 6))
+# line1 = ax1.plot((co2_captured_cement + co2_captured_w2e) / (emission_cement + emission_w2e),
+#                  label='CO2 Captured/Total Emissions', color='blue', marker='o', markersize=3, linewidth=0.7)
+# ax1.set_xlabel('Time')
+# ax1.set_ylabel('CO2 Captured/Total Emissions', color='blue')
+# ax1.tick_params(axis='y', labelcolor='blue')
+# ax2 = ax1.twinx()
+# line2 = ax2.plot((co2_captured_cement + co2_captured_w2e),
+#                  label='Total CO2 Captured', color='green', marker='x', markersize=3, linewidth=0.7, linestyle='--')
+# ax2.set_ylabel('Total CO2 Captured', color='green')
+# ax2.tick_params(axis='y', labelcolor='green')
+# plt.title('Share of Total CO2 Captured')
+# ax1.grid(True)
+# lines = line1 + line2
+# labels = [line.get_label() for line in lines]
+# ax1.legend(lines, labels, loc='lower right')
+# ax1.set_xlim(0, 1800)
+# plt.show()
 
 
 fig = plt.figure(figsize=(10, 6))
 # Fill the area under the captured CO2 curve (Light Pink from Crameri Batlow)
-plt.fill_between(days/365, 0, tot_co2_captured, color='#F6C6D6', alpha=0.7, label='Captured CO$_2$')
+plt.fill_between(days/365, 0, tot_co2_captured, color='#D491B8', alpha=0.7, label='Captured CO$_2$')
 # Fill the area between captured and emitted CO2 (Dark Blue from Crameri Batlow)
 plt.fill_between(days/365, tot_co2_captured, emission_tot, color='#012E4D', alpha=0.7, label='Emitted CO$_2$')
 plt.xlabel('Time [year]')
@@ -146,7 +146,8 @@ save_figure_for_paper(fig, "emissions", path_plot)
 
 batlow_colors = ['#222A6A', '#4B708A', '#6FBC7B', '#B1E87E', '#F7D03C']
 fig1, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
-ax1.plot(days/365, average_inj_rate, color='#F6C6D6', linewidth=2, label='Injection Rate')
+ax1.plot(days/365, average_inj_rate, color='#D491B8', linewidth=2, label='Average injection Rate')
+
 ax1.set_ylabel('Average injection rate [t/day]')  # Replace 'units' with the appropriate unit for injection rate
 ax1.legend()
 ax1.set_ylim(max(average_inj_rate)*0.6, max(average_inj_rate) * 1.1)
@@ -179,31 +180,30 @@ p_pump_in = data_loaded["p_pump_in"]
 p_pump_out = whp + p_loss_offshore
 pump_unfitted_power = tot_co2_captured * nu * (p_pump_out - p_pump_in) / eta_pump * 0.1 / 3.6  # power in MWh/day
 ratio_fit_pump = power_pump/pump_unfitted_power
-fixed_pump_power = pump_unfitted_power[0]
+fixed_pump_power = tot_co2_captured * nu * (93.563 - p_pump_in) / eta_pump * 0.1 / 3.6
 pump_ratio = power_pump/fixed_pump_power
-averaged_pump_ratio = [np.mean(pump_ratio[i:i+180]) for i in range(0, len(pump_ratio), 180)]
-expanded_pump_ratio = np.repeat(averaged_pump_ratio, 180)
+
 
 
 fig3 = plt.figure(figsize=(10, 6))
-plt.plot(days/365, expanded_pump_ratio, color='#66B2A5', linewidth=2, label='Pressure-dependent')
+plt.plot(days/365, pump_ratio, color='#66B2A5', linewidth=2, label='Pressure-dependent')
 plt.axhline(y=1.0, color="#082D48", linestyle='--', linewidth=1, label='Static')
 plt.xlabel('Time [year]')
 plt.ylabel('Normalized specific pump consumption')
-plt.ylim(0, max(power_pump/fixed_pump_power)*1.1)
-plt.title('Impact of bhp variations on the pump power consumption')
-plt.legend()
+plt.ylim(0.5, max(power_pump/fixed_pump_power)*1.1)
+# plt.title('Impact of bhp variations on the pump power consumption')
+plt.legend(loc='lower right')
 plt.tight_layout()
 plt.show(block=False)
 save_figure_for_paper(fig3, "power_pump", path_plot)
 
 fig2 = plt.figure(figsize=(10, 6))
-plt.plot(days/365, ratio_fit_pump, color='#294B6C', linewidth=2, label='Fitted pump power over real one')
-plt.axhline(y=1.0, color="#DCE391", linestyle='--', linewidth=1, label='Perfect fit')
+plt.plot(days/365, ratio_fit_pump, color='#66B2A5', linewidth=2)
+plt.axhline(y=1.0, color="black", linestyle='--', linewidth=1, label='Perfect fit')
 plt.xlabel('Time [year]')
 plt.ylabel('Ratio')
 plt.ylim(0.6, max(ratio_fit_pump) * 1.1)
-plt.title('Quality of the pump fit')
+# plt.title('Quality of the pump fit')
 plt.legend()
 plt.tight_layout()
 plt.show(block=False)
@@ -211,18 +211,22 @@ save_figure_for_paper(fig2, "fit_pump", path_plot)
 
 # Parity plot: Predicted vs Actual
 
-fig4 = plt.figure(figsize=(10, 6))
-plt.scatter(pump_unfitted_power, power_pump, color='#294B6C', alpha=0.7, label='Data Points')
-plt.plot([0, max(pump_unfitted_power)], [0, max(pump_unfitted_power)], color="#DCE391", linestyle='--', linewidth=1.5, label='Perfect Fit')
-plt.plot([0, max(pump_unfitted_power)], [0, max(pump_unfitted_power) * 1.1], color='gray', linestyle='--', linewidth=1, label='+10% Uncertainty')
-plt.plot([0, max(pump_unfitted_power)], [0, max(pump_unfitted_power) * 0.9], color='gray', linestyle='--', linewidth=1, label='-10% Uncertainty')
-plt.xlabel('Actual Pump Power Consumption')
-plt.ylabel('Predicted Pump Power Consumption')
-plt.title('Parity Plot: Predicted vs Actual Pump Power Consumption with ±10% Uncertainty')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-save_figure_for_paper(fig4, "parity_pump_fit", path_plot)
+# fig4 = plt.figure(figsize=(10, 6))
+# plt.scatter(pump_unfitted_power, power_pump, color=batlow_colors[2], s=30, alpha=0.7, label='Data Points')
+# plt.plot([0, max(pump_unfitted_power)], [0, max(pump_unfitted_power)], color="black", linestyle='--', linewidth=1.5, label='Perfect Fit')
+# plt.plot([0, max(pump_unfitted_power)], [0, max(pump_unfitted_power) * 1.1], color='gray', linestyle='--', linewidth=1)
+# plt.plot([0, max(pump_unfitted_power)], [0, max(pump_unfitted_power) * 0.9], color='gray', linestyle='--', linewidth=1)
+#
+# # Text annotations for uncertainty lines
+# plt.text(max(pump_unfitted_power) * 0.8, max(pump_unfitted_power) * 1.1, '+10%', color='gray', fontsize=12, ha='left')
+# plt.text(max(pump_unfitted_power) * 0.8, max(pump_unfitted_power) * 0.9, '-10%', color='gray', fontsize=12, ha='left')
+# plt.xlabel('Actual Pump Power Consumption')
+# plt.ylabel('Predicted Pump Power Consumption')
+# plt.title('Parity Plot: Predicted vs Actual Pump Power Consumption with ±10% Uncertainty')
+# plt.legend()
+# plt.tight_layout()
+# save_figure_for_paper(fig4, "parity_pump_fit", path_plot)
+
 
 plt.show()
 
