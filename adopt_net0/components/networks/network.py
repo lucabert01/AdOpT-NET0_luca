5fe5bdc1a8b9ee1a327e8f16cc60a2916ea97c47
Jean <62931644+JeanWi@users.noreply.github.com>
2025-04-29 13:39:08 +0200

    Removed rated capacity again (wrong)

--- adopt_net0/components/networks/network.py
+++ adopt_net0/components/networks/network.py
@@ -613,9 +613,9 @@ class Network(ModelComponent):
         def calculate_max_capex():
             max_capex = (
                 b_netw.para_capex_gamma1
-                + b_netw.para_capex_gamma2 * b_arc.para_size_max * rated_capacity
+                + b_netw.para_capex_gamma2 * b_arc.para_size_max
                 + b_netw.para_capex_gamma3 * b_arc.distance
-                + b_netw.para_capex_gamma4 * b_arc.para_size_max * b_arc.distance * rated_capacity
+                + b_netw.para_capex_gamma4 * b_arc.para_size_max * b_arc.distance
             )
             return (0, max_capex)
 
@@ -648,9 +648,9 @@ class Network(ModelComponent):
             return (
                 b_arc.var_capex_aux
                 == b_netw.para_capex_gamma1
-                + b_netw.para_capex_gamma2 * b_arc.var_size * rated_capacity
+                + b_netw.para_capex_gamma2 * b_arc.var_size
                 + b_netw.para_capex_gamma3 * b_arc.distance
-                + b_netw.para_capex_gamma4 * b_arc.var_size * b_arc.distance* rated_capacity
+                + b_netw.para_capex_gamma4 * b_arc.var_size * b_arc.distance
             )
 
         # CAPEX aux:
