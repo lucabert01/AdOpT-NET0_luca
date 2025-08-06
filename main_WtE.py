65e044c3831693f9fb320bb6366710a27db362fd
lucabert01 <l.bertoni@uu.nl>
2025-08-06 17:06:43 +0200

    Loop over multiple DH ratio

--- main_WtE.py
+++ main_WtE.py
@@ -8,161 +8,174 @@ from adopt_net0.data_preprocessing import load_climate_data_from_api
 
 
 # Specify the path to your input data
-path = Path("./CaseStudy_WtE")
+casepath= Path("CaseStudy_WtE_DHratio_minEmission")
 json_files_path = Path("./dataCaseStudy_WtE/technologies_json")
+result_path = "./dataCaseStudy_WtE/raw_results"
+
+adopt.create_optimization_templates(casepath)
+
 
 # General input data
-# TODO add the correct WtE plants' profiles
 possible_plants = ["silla2", "gerbido", "PAIP", "piacenza"]
 plant_analyzed = possible_plants[0]
 carbon_tax = 200
-fraction_peak_heat_demand = 0.5 # fraction of peak heat demand to supply compared to peak heat prod. from WtE
+max_dh_ratio = [0, 0.25, 0.5, 0.75, 1] # ratio of peak DH demand to supply compared to peak heat prod. from WtE
+pyhub = {}
 
-# Create template files (comment these lines if already defined)
-adopt.create_optimization_templates(path)
+for dh_ratio in max_dh_ratio:
+    pyhub_dh_ratio = f"dh_ratio_{dh_ratio}"
+    # Load json template
+    with open(casepath / "Topology.json", "r") as json_file:
+        topology = json.load(json_file)
+    # Nodes
+    topology["nodes"] = ["industrial_cluster"]
+    # Carriers:
+    topology["carriers"] = [
+        "electricity",
+        "CO2captured",
+        "heat",
+        "wasteFuel",
+        "wasteProcessed",
+        "gas",
+    ]
+    # Investment periods:
+    topology["investment_periods"] = ["period1"]
+    # Save json template
+    with open(casepath / "Topology.json", "w") as json_file:
+        json.dump(topology, json_file, indent=4)
+    
+    end_period = 8760
+    
+    
+    # Load json template
+    with open(casepath / "ConfigModel.json", "r") as json_file:
+        configuration = json.load(json_file)
+    # Change objective
+    configuration["optimization"]["objective"]["value"] = "emissions_net"
+    # Set MILP gap
+    configuration["solveroptions"]["mipgap"]["value"] = 0.02
+    # change save options
+    configuration['reporting']['save_summary_path']['value'] = result_path
+    configuration['reporting']['save_path']['value'] = result_path
 
-# Load json template
-with open(path / "Topology.json", "r") as json_file:
-    topology = json.load(json_file)
-# Nodes
-topology["nodes"] = ["industrial_cluster"]
-# Carriers:
-topology["carriers"] = [
-    "electricity",
-    "CO2captured",
-    "heat",
-    "wasteFuel",
-    "wasteProcessed",
-    "gas",
-]
-# Investment periods:
-topology["investment_periods"] = ["period1"]
-# Save json template
-with open(path / "Topology.json", "w") as json_file:
-    json.dump(topology, json_file, indent=4)
+    # Save json template
+    with open(casepath / "ConfigModel.json", "w") as json_file:
+        json.dump(configuration, json_file, indent=4)
+    
+    adopt.create_input_data_folder_template(casepath)
+    
+    node_location = pd.read_csv(casepath / "NodeLocations.csv", sep=";", index_col=0, header=0)
+    node_location.at["industrial_cluster", "lon"] = 10
+    node_location.at["industrial_cluster", "lat"] = 10
+    node_location.at["industrial_cluster", "alt"] = 10
+    node_location = node_location.reset_index()
+    node_location.to_csv(casepath / "NodeLocations.csv", sep=";", index=False)
+    
+    with open(
+        casepath / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "r"
+    ) as json_file:
+        technologies = json.load(json_file)
+    technologies["new"] = ["WasteCHP", "Boiler_Industrial_NG"]
+    
+    with open(
+        casepath / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "w"
+    ) as json_file:
+        json.dump(technologies, json_file, indent=4)
+    
+    # Copy over technology files
+    adopt.copy_technology_data(casepath, json_files_path)
+    
+    
+    # Import hourly profiles
+    path_processed_data = Path("./dataCaseStudy_WtE/dataSources/hourly_data_casestudy.xlsx")
+    data = pd.read_excel(path_processed_data)
+    electricity_price = data["el_price_itNord"]
+    emissions = data[f"emission_{plant_analyzed}"]
+    norm_heat_demand = data["normalized_heat_demand_milan"]
+    json_wasteCHP = Path("./dataCaseStudy_WtE/technologies_json/WasteCHP.json")
+    info_wasteCHP = json.loads(json_wasteCHP.read_text())
+    lhv = info_wasteCHP["Performance"]["LHV"]
+    emission_factor = info_wasteCHP["Performance"]["emission_factor"]
+    wasteProcessed_demand = emissions/emission_factor
+    max_heat_output = max(wasteProcessed_demand)*lhv
+    peak_heat_demand = dh_ratio*max_heat_output
+    heat_demand = norm_heat_demand * peak_heat_demand
+    
+    # Set import limits/cost
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=5000,
+        columns=["Export limit"],
+        carriers=["electricity"],
+        nodes=["industrial_cluster"],
+    )
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=5000,
+        columns=["Export limit"],
+        carriers=["CO2captured"],
+        nodes=["industrial_cluster"],
+    )
+    
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=electricity_price,
+        columns=["Export price"],
+        carriers=["electricity"],
+        nodes=["industrial_cluster"],
+    )
+    
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=1000,
+        columns=["Import limit"],
+        carriers=["wasteFuel"],
+        nodes=["industrial_cluster"],
+    )
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=1000,
+        columns=["Import limit"],
+        carriers=["gas"],
+        nodes=["industrial_cluster"],
+    )
+    
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=wasteProcessed_demand,
+        columns=["Demand"],
+        carriers=["wasteProcessed"],
+        nodes=["industrial_cluster"],
+    )
+    adopt.fill_carrier_data(
+        casepath,
+        value_or_data=heat_demand,
+        columns=["Demand"],
+        carriers=["heat"],
+        nodes=["industrial_cluster"],
+    )
+    
+    
+    carbon_price = np.ones(8760) * carbon_tax
+    carbon_cost_path = (
+        casepath / "period1" / "node_data" / "industrial_cluster" / "CarbonCost.csv"
+    )
+    carbon_cost_template = pd.read_csv(carbon_cost_path, sep=";", index_col=0, header=0)
+    carbon_cost_template["price"] = carbon_price
+    carbon_cost_template = carbon_cost_template.reset_index()
+    carbon_cost_template.to_csv(carbon_cost_path, sep=";", index=False)
+    
+    load_climate_data_from_api(folder_path=casepath)
+    
+    # Construct and solve the model
+    pyhub[pyhub_dh_ratio] = adopt.ModelHub()
+    pyhub[pyhub_dh_ratio].read_data(casepath, start_period=0, end_period=end_period)
 
-end_period = 8760
-
-
-# Load json template
-with open(path / "ConfigModel.json", "r") as json_file:
-    configuration = json.load(json_file)
-# Change objective
-configuration["optimization"]["objective"]["value"] = "costs"
-# Set MILP gap
-configuration["solveroptions"]["mipgap"]["value"] = 0.02
-# Save json template
-with open(path / "ConfigModel.json", "w") as json_file:
-    json.dump(configuration, json_file, indent=4)
-
-adopt.create_input_data_folder_template(path)
-
-node_location = pd.read_csv(path / "NodeLocations.csv", sep=";", index_col=0, header=0)
-node_location.at["industrial_cluster", "lon"] = 10
-node_location.at["industrial_cluster", "lat"] = 10
-node_location.at["industrial_cluster", "alt"] = 10
-node_location = node_location.reset_index()
-node_location.to_csv(path / "NodeLocations.csv", sep=";", index=False)
-
-with open(
-    path / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "r"
-) as json_file:
-    technologies = json.load(json_file)
-technologies["new"] = ["WasteCHP", "Boiler_Industrial_NG"]
-
-with open(
-    path / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "w"
-) as json_file:
-    json.dump(technologies, json_file, indent=4)
-
-# Copy over technology files
-adopt.copy_technology_data(path, json_files_path)
-
-
-# Import hourly profiles
-path_processed_data = Path("./dataCaseStudy_WtE/dataSources/hourly_data_casestudy.xlsx")
-data = pd.read_excel(path_processed_data)
-electricity_price = data["el_price_itNord"]
-emissions = data[f"emission_{plant_analyzed}"]
-norm_heat_demand = data["normalized_heat_demand_milan"]
-json_wasteCHP = Path("./dataCaseStudy_WtE/technologies_json/WasteCHP.json")
-info_wasteCHP = json.loads(json_wasteCHP.read_text())
-lhv = info_wasteCHP["Performance"]["LHV"]
-emission_factor = info_wasteCHP["Performance"]["emission_factor"]
-wasteProcessed_demand = emissions/emission_factor
-max_heat_output = max(wasteProcessed_demand)*lhv
-peak_heat_demand = fraction_peak_heat_demand*max_heat_output
-heat_demand = norm_heat_demand * peak_heat_demand
-
-# Set import limits/cost
-adopt.fill_carrier_data(
-    path,
-    value_or_data=5000,
-    columns=["Export limit"],
-    carriers=["electricity"],
-    nodes=["industrial_cluster"],
-)
-adopt.fill_carrier_data(
-    path,
-    value_or_data=5000,
-    columns=["Export limit"],
-    carriers=["CO2captured"],
-    nodes=["industrial_cluster"],
-)
-
-adopt.fill_carrier_data(
-    path,
-    value_or_data=electricity_price,
-    columns=["Export price"],
-    carriers=["electricity"],
-    nodes=["industrial_cluster"],
-)
-
-adopt.fill_carrier_data(
-    path,
-    value_or_data=1000,
-    columns=["Import limit"],
-    carriers=["wasteFuel"],
-    nodes=["industrial_cluster"],
-)
-adopt.fill_carrier_data(
-    path,
-    value_or_data=1000,
-    columns=["Import limit"],
-    carriers=["gas"],
-    nodes=["industrial_cluster"],
-)
-
-adopt.fill_carrier_data(
-    path,
-    value_or_data=wasteProcessed_demand,
-    columns=["Demand"],
-    carriers=["wasteProcessed"],
-    nodes=["industrial_cluster"],
-)
-adopt.fill_carrier_data(
-    path,
-    value_or_data=heat_demand,
-    columns=["Demand"],
-    carriers=["heat"],
-    nodes=["industrial_cluster"],
-)
-
-
-carbon_price = np.ones(8760) * carbon_tax
-carbon_cost_path = (
-    path / "period1" / "node_data" / "industrial_cluster" / "CarbonCost.csv"
-)
-carbon_cost_template = pd.read_csv(carbon_cost_path, sep=";", index_col=0, header=0)
-carbon_cost_template["price"] = carbon_price
-carbon_cost_template = carbon_cost_template.reset_index()
-carbon_cost_template.to_csv(carbon_cost_path, sep=";", index=False)
-
-load_climate_data_from_api(folder_path=path)
-
-# Construct and solve the model
-m = adopt.ModelHub()
-m.read_data(path, start_period=0, end_period=end_period)
-m.construct_model()
-m.construct_balances()
-m.solve()
+    pyhub[pyhub_dh_ratio].data.model_config['reporting']['case_name'][
+        'value'] = pyhub_dh_ratio
+    pyhub[pyhub_dh_ratio].data.time_series['full']['period1', 'industrial_cluster', 'CarrierData', 'heat', 'Demand'] = heat_demand
+    
+    
+    pyhub[pyhub_dh_ratio].construct_model()
+    pyhub[pyhub_dh_ratio].construct_balances()
+    pyhub[pyhub_dh_ratio].solve()
---
+++ run_wte_mea_dhratio.py
@@ -0,0 +1,84 @@
+#Define basepath
+basepath = os.path.dirname(os.path.abspath(__file__))
+
+#Run Chemelot emission limit
+execute = 1
+
+if execute == 1:
+    # Specify the path to your input data
+    casepath = os.path.join(basepath, "Case_studies", "MY_Chemelot_gf_")
+    resultpath = os.path.join(basepath, "raw_results", "EmissionLimit Greenfield")
+
+    # select simulation types
+    node = 'Chemelot'
+    scope3 = 1
+    run_with_emission_limit = 1
+    intervals = ['2040']
+    # intervals = ['2030', '2040', '2050']
+    interval_emissionLim = {'2030': 1, '2040': 0.5, '2050': 0}
+    nr_DD_days = 10
+    take_prev_solution = 1
+    prev_emission = 522537.2155
+    pyhub = {}
+
+    for i, interval in enumerate(intervals):
+        casepath_interval = casepath + interval
+        json_filepath = Path(casepath_interval) / "ConfigModel.json"
+
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
+
+        model_config['optimization']['typicaldays']['N']['value'] = nr_DD_days
+
+        if interval == '2030':
+            model_config['optimization']['objective']['value'] = 'costs'
+        else:
+            prev_interval = intervals[i - 1]
+            model_config['optimization']['objective']['value'] = "costs_emissionlimit"
+            if interval == '2040' and take_prev_solution:
+                limit = interval_emissionLim[interval] * prev_emission
+            else:
+                if nr_DD_days > 0:
+                    limit = interval_emissionLim[interval] * pyhub[prev_interval].model['clustered'].var_emissions_net.value
+                else:
+                    limit = interval_emissionLim[interval] * pyhub[prev_interval].model['full'].var_emissions_net.value
+            model_config['optimization']['emission_limit']['value'] = limit
+
+        # Scope 3 analysis yes/no
+        model_config['optimization']['scope_three_analysis'] = scope3
+
+        # solver settings
+        model_config['solveroptions']['timelim']['value'] = 24*30
+        model_config['solveroptions']['mipgap']['value'] = 0.01
+        model_config['solveroptions']['threads']['value'] = 12
+        model_config['solveroptions']['nodefilestart']['value'] = 200
+
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath + node
+        model_config['reporting']['save_path']['value'] = resultpath + node
+
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
+
+        # Construct and solve the model
+        pyhub[interval] = ModelHub()
+        pyhub[interval].read_data(casepath_interval)
+
+        # Set case name
+        if nr_DD_days > 0:
+            pyhub[interval].data.model_config['reporting']['case_name'][
+                'value'] = (interval + '_minC_' +
+                            'DD' + str(pyhub[interval].data.model_config['optimization']['typicaldays']['N']['value']))
+            pyhub[interval].data.time_series['clustered'][
+                interval, node, 'CarbonCost', 'global', 'price'] = 150.31
+        else:
+            pyhub[interval].data.model_config['reporting']['case_name'][
+                'value'] = interval + '_minC_fullres'
+
+        pyhub[interval].data.time_series['full'][interval, node, 'CarbonCost', 'global', 'price'] = 150.31
+
+        # Start brownfield optimization
+        pyhub[interval].construct_model()
+        pyhub[interval].construct_balances()
+        pyhub[interval].solve()
