2a4486a6f933de349d3ba9d2c430614d2aa849c8
julia1071 <j.l.tiggeloven@uu.nl>
2025-10-16 11:39:59 +0200

    Autoformatting

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -341,11 +341,14 @@ def test_network_decommission(request):
 
     m = construct_netw_model(netw, nr_timesteps)
 
-    #run model
+    # run model
     run_model(m, request.config.solver, objective="capex")
 
     # No decommissioning
-    assert m.arc_block["node1", "node2"].var_size.value == size_initial.loc["node1", "node2"]
+    assert (
+        m.arc_block["node1", "node2"].var_size.value
+        == size_initial.loc["node1", "node2"]
+    )
 
     # Technology can decommission
     netw = define_network(
