5560481a2fe39352f7f078864b27629d9e205155
lucabert01 <l.bertoni@uu.nl>
2025-12-08 14:20:17 +0100

    Added more range to el prices in tech_selection to see when CaL is selected

--- main_WtE_technology_selection.py
+++ main_WtE_technology_selection.py
@@ -26,7 +26,7 @@ data = pd.read_excel(path_processed_data)
 # General input data
 objective_function = "costs" # "emissions_net", "emissions_minC", "costs"
 explored_carbon_tax = [50, 75, 100,125, 150]
-explored_el_price = [25, 50, 75, 100,125] # average el prices explored in the analysis
+explored_el_price = [25, 50, 75, 100,125, 150, 175] # average el prices explored in the analysis
 explored_dh_ratio = [0.5, 0.75]
 plant_analyzed = "PAIP" # one between: "silla2", "gerbido", "PAIP", "piacenza"
 gas_price = 40
