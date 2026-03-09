4cc1b481120ac8126f596f141cff8c5532535d32
lucabert01 <l.bertoni@uu.nl>
2026-03-09 22:01:18 +0100

    results cement tech selection updated 2.0

--- main_cement.py
+++ main_cement.py
@@ -20,9 +20,7 @@ objective_function = "costs" # "emissions_net", "emissions_minC", "costs"
 possible_plants = ["Vernasca", "Robilante", "Monselice", "Fanna"]
 plant_analyzed = "Vernasca"
 explored_carbon_tax = [50, 100, 150, 200]
-explored_carbon_tax = [50]
 explored_el_price = [50, 100, 150, 200] # average el prices explored in the analysis
-explored_el_price = [50] # average el prices explored in the analysis
 distance_to_stor = 100
 dymanics_on = 0
 
@@ -106,7 +104,7 @@ for carbon_tax in explored_carbon_tax:
             casepath / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "r"
         ) as json_file:
             technologies = json.load(json_file)
-        technologies["new"] = [ "CementEmitter", "HeatPump"]
+        technologies["new"] = ["CementHybridCCS", "CementEmitter", "HeatPump"]
 
         with open(
             casepath / "period1" / "node_data" / "industrial_cluster" / "Technologies.json", "w"
