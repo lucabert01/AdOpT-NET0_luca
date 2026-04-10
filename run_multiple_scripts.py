442110789a3abd50e0831a40387483795860647b
lucabert01 <l.bertoni@uu.nl>
2026-04-10 15:30:35 +0200

    Results wte tech selection

--- run_multiple_scripts.py
+++ run_multiple_scripts.py
@@ -1,7 +1,7 @@
 import subprocess, os, sys
 
 mains = ["main_cement.py", "main_WtE_CaL.py", "main_WtE_MEA.py", "main_WtE_MEA_timeless.py", "main_WtE_technology_selection.py"]
-mains = ["main_WtE_MEA.py", "main_WtE_MEA_timeless.py", "main_WtE_technology_selection.py"]
+mains = ["main_WtE_MEA.py", "main_WtE_MEA_timeless.py"]
 plots = ["plot_results_CaL.py","plot_results_MEA.py","plot_results_WtE_technology_selection.py","plot_results_cement.py",]
 processes = []
 env = os.environ.copy()
